edb9dffa3cbfd49e70bb8dd771c1353944ebb814
avishai220 <avishai220@walla.com>
2026-08-29 23:15:45 +0300

    Update products_page.py

--- pages/products_page.py
+++ pages/products_page.py
@@ -58,6 +58,7 @@ class ProductsPage(BasePage):
     def add_product_by_index(self, index):
         self.hover_by_index(self.__PRODUCT_CARD, index)
         self.click_by_index_force(self.__ADD_TO_CART_BTN, index)
+        self.wait_time_visible(self.__CONTINUE_SHOPPING)
 
     def continue_shopping(self):
         self.click(self.__CONTINUE_SHOPPING)
